a6012f1cf9debb9e3d089a21f21cb0be535fa27f
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-09-26 09:54:50 +0600

    add tailwind to project

---
+++ src/components/Product.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Product() {
+    return (
+        <div className='border py-2 px-4 rounded flex-col text-center items-center mb-2'>Product</div>
+    )
+}
